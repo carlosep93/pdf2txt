b8ca6550c9948bbc81d4206e138f12f929bbcd21
carlosep93 <carlos.e.p93@gmail.com>
2024-10-21 17:18:03 +0200

    add nllb download

---
+++ pipeline/translate-nllb.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=translate_xml
+#SBATCH --output=slurm_logs/translate_%j.out
+#SBATCH --error=slurm_logs/translate_%j.err
+#SBATCH --gres=gpu:1
+#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=20
+#SBATCH --qos acc_bscls
+#SBATCH -N1
+#SBATCH --account bsc88
+##SBATCH --qos=acc_bscls
+##SBATCH --dependency=afterok:4588757
+
+
+source /gpfs/scratch/bsc88/bsc088662/envs/use_env_nextpro.sh
+
+umask 007
+
+model_dir='/gpfs/scratch/bsc88/bsc088662/nextpro/model/nllb-200-distilled-600M/'
+data_dir='/gpfs/projects/bsc88/data/03-text-repository/03.2-processing/clean/gl'
+output_dir='output/gl-es'
+src_lang='glg_Latn'
+tgt_lang='spa_Latn'
+tok_lang='Galician'
+
+tsv_file='nextprocurement_mx_20240626_16.clean'
+
+python translate_tsv_xml_nllb.py \
+    --model_dir $model_dir \
+    --data_dir  $data_dir \
+    --output_dir $output_dir \
+    --tsv_file $tsv_file \
+    --src_lang $src_lang \
+    --tgt_lang $tgt_lang \
+    --tok_lang $tok_lang 
+
+
---
+++ pipeline/translate_tsv_xml_nllb.py
@@ -0,0 +1,128 @@
+from bs4 import BeautifulSoup
+import ctranslate2
+import transformers
+from texttokenizer import TextTokenizer
+import pandas as pd
+import unicodedata
+import os
+import argparse
+
+def _normalize_input_string(result):
+    result = unicodedata.normalize('NFC', result)
+    return result
+
+def _translate_batch(input_batch, spm, model, max_sentence_batch=10):
+
+    batch_input_tokenized = []
+    batch_input_markers = []
+
+    #preserve_markup = PreserveMarkup()
+
+    num_sentences = len(input_batch)
+    for pos in range(0, num_sentences):
+        tokenized = spm.convert_ids_to_tokens(spm.encode(input_batch[pos]))
+        batch_input_tokenized.append(tokenized)
+
+    batch_output = []
+    for offset in range(0,len(batch_input_tokenized), max_sentence_batch):
+      batch = batch_input_tokenized[offset:offset+max_sentence_batch]
+      partial_result = model.translate_batch(batch, 
+                                            return_scores=False, 
+                                            replace_unknowns=True, 
+                                            target_prefix=[target_prefix]*len(batch))
+      for pos in range(0,len(partial_result)):
+        tokenized = partial_result[pos][0]['tokens'][1:]
+        translated = spm.decode(spm.convert_tokens_to_ids(tokenized))
+        print(translated)
+        batch_output.append(translated)
+
+    return batch_output
+
+def translate(text):
+  sentences, translate = tokenizer.tokenize(text)
+  num_sentences = len(sentences)
+  sentences_batch = []
+  indexes = []
+  results = ["" for x in range(num_sentences)]
+  for i in range(num_sentences):
+      if translate[i] is False:
+          continue
+
+      sentences_batch.append(sentences[i])
+      indexes.append(i)
+
+  translated_batch = _translate_batch(sentences_batch, spm, translator, max_sentence_batch)
+  for pos in range(0, len(translated_batch)):
+      i = indexes[pos]
+      results[i] = translated_batch[pos]
+
+  #Rebuild split sentences
+  translated = tokenizer.sentence_from_tokens(sentences, translate, results)
+  return translated
+
+
+def translate_xml_tree(xml_string, tags=['heading','p']):
+  xml_string = xml_string.replace('\\n','\n')
+  xml_tree = BeautifulSoup(xml_string, features="xml")
+  for tag in tags:
+    instances = xml_tree.findAll(tag)
+    for  ins in instances:
+        text = ins.getText()
+        ins.string = translate(text)
+  return str(xml_tree).replace('\n','\\n')
+
+def get_out_file(data_file, out_dir):
+    data_file = str(data_file).split('/')[-1]
+    data_file = data_file.split('.')[0]
+    return out_dir + '/' + data_file + '.tr'
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_dir", help="Directory where the model is stored.")
+parser.add_argument("--data_dir", help="Directory where the data is stored.")
+parser.add_argument("--output_dir", help="Directory where the output is stored.")
+parser.add_argument("--tsv_file", help="Name of the .tsv file to translate.")
+parser.add_argument("--src_lang", help="Source language in NLLB format.")
+parser.add_argument("--tgt_lang", help="Target language in NLLB format.")
+parser.add_argument("--tok_lang", help="Language used for pretokenization. Full Word in English.")
+args = parser.parse_args()
+
+
+
+#Init model to translate
+model_dir=args.model_dir
+language=args.tok_lang
+spm=transformers.AutoTokenizer.from_pretrained(model_dir, src_lang=args.src_lang)
+translator = ctranslate2.Translator(model_dir, device="cuda")
+tokenizer=TextTokenizer(language)
+
+
+#Load data from TSV
+data_dir=args.data_dir
+out_dir=args.output_dir
+target_prefix=[args.tgt_lang]
+max_sentence_batch=40
+input_field='text'
+output_field='translation'
+
+
+f = data_dir + '/' + args.tsv_file
+print('Reading file:', f)
+out_file = get_out_file(f,out_dir)
+
+if os.path.isfile(out_file):
+    print('File already processed:', f)
+    exit(0)
+
+df = pd.read_csv(f, sep='\t', encoding='utf-8', on_bad_lines='skip')
+df[output_field] = pd.Series(dtype='str')
+for row_index,row in df.iterrows():
+    print ('Translating sentece', row_index, "out of", len(df))
+    text = _normalize_input_string(row[input_field].strip())
+    df.loc[row_index,[output_field]] = translate_xml_tree(text)
+
+print('Writing output file:', out_file)
+df.to_csv(out_file, sep = '\t')
+
+
